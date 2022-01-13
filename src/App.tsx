51a0b2e9234711d025194947a8d4152ba8bad485
joe-main <qmmplusfact@gmail.com>
2022-01-13 21:39:42 +0900

    Banner/Nav用のcomponentとscss追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import './App.css';
+import { Nav } from "./compornents/Nav";
 import { Row } from "./compornents/Row";
+import { Banner } from "./compornents/Banner";
 import { requests } from "./request";
 
 function App() {
   return (
     <div className="App">
+    <Nav />
+    <Banner />
     <Row
       title="NETFLIX ORIGUINALS"
       fetchUrl={requests.feachNetflixOriginals}
--- src/compornents/Row.tsx
+++ src/compornents/Row.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from '../axios'
+import "./Row.scss";
+
+const base_url = "https://image.tmdb.org/t/p/original";
 
 type Props = {
   title: string;
@@ -17,7 +20,7 @@ type Movie = {
 };
 
 
-export const Row = ({ title, fetchUrl }: Props) => {
+export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([]);
   useEffect(() => {
     async function fetchData() {
@@ -29,9 +32,21 @@ export const Row = ({ title, fetchUrl }: Props) => {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
-
   return(
-      <div className="Row" /> 
+      <div className="Row"> 
+        <h2>{title}</h2>
+          <div className="Row-posters">
+            {movies.map((movie, i) => (
+              <img
+                key={movie.id}
+                className={`Row-poster ${isLargeRow && "Row-poster-large"}`}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            ))}
+          </div>
+      </div>
   );
 };
